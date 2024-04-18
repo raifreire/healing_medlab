b42ab4aa31930a6160f494cceb563bf002819a43
Rai Freire <rai.freire26@gmail.com>
2024-04-18 15:54:16 -0300

    add consultas

--- paciente/views.py
+++ paciente/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from medico.models import DadosMedico, Especialidades
+from django.shortcuts import render,redirect
+from medico.models import DadosMedico, Especialidades, DatasAbertas
+from datetime import datetime
+from .models import Consulta
+from django.contrib import messages
+from django.contrib.messages import constants
 
 def home(request):
     if request.method == "GET":
@@ -16,3 +20,40 @@ def home(request):
             medicos = medicos.filter(especialidade_id__in=especialidades_filtrar)
         
         return render(request, 'home.html',{'medicos':medicos,'especialidades':especialidades})
+
+
+def escolher_horario(request, id_dados_medicos):
+    if request.method == "GET":
+        medico = DadosMedico.objects.get(id=id_dados_medicos)
+        datas_abertas = DatasAbertas.objects.filter(user=medico.user).filter(data__gte=datetime.now()).filter(agendado=False)
+        return render(request, 'escolher_horario.html', {'medico': medico, 'datas_abertas': datas_abertas})
+    
+
+def agendar_horario(request, id_data_aberta):
+    if request.method == "GET":
+        data_aberta = DatasAbertas.objects.get(id=id_data_aberta)
+
+        horario_agendado = Consulta(
+            paciente=request.user,
+            data_aberta=data_aberta
+        )
+
+        horario_agendado.save()
+
+        # TODO: Sugestão Tornar atomico
+
+        data_aberta.agendado = True
+        data_aberta.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Horário agendado com sucesso.')
+
+        return redirect('/pacientes/minhas_consultas/')
+    
+
+def minhas_consultas(request):
+    if request.method == "GET":
+        #TODO: desenvolver filtros
+        minhas_consultas = Consulta.objects.filter(paciente=request.user).filter(data_aberta__data__gte=datetime.now())
+        return render(request, 'minhas_consultas.html', {'minhas_consultas': minhas_consultas})
+
+
