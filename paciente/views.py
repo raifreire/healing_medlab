0e62e45db1ff501625a14869223912dc141a157d
Rai <rai.freire26@gmail.com>
2024-04-17 22:46:36 -0300

    add filtros

--- paciente/views.py
+++ paciente/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from medico.models import DadosMedico,DatasAbertas
+from medico.models import DadosMedico, Especialidades
 
 def home(request):
     if request.method == "GET":
         medicos = DadosMedico.objects.all()
-        return render(request, 'home.html',{'medicos':medicos})
+        especialidades = Especialidades.objects.all()
+        
+        medico_filtrar = request.GET.get('medico')
+        especialidades_filtrar = request.GET.getlist('especialidades')
+
+        if medico_filtrar:
+            medicos = medicos.filter(nome__icontains = medico_filtrar)
+
+        if especialidades_filtrar:
+            medicos = medicos.filter(especialidade_id__in=especialidades_filtrar)
+        
+        return render(request, 'home.html',{'medicos':medicos,'especialidades':especialidades})
